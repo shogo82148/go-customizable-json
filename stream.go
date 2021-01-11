dd09940f92b2e2d97cdebacdee32d3f129192372
Ichinose Shogo <shogo82148@gmail.com>
2021-01-11 19:59:47 +0900

    fix Token

--- stream.go
+++ stream.go
@@ -53,7 +53,17 @@ func (dec *Decoder) More() bool {
 // Token returns the next JSON token in the input stream.
 // At the end of the input stream, Token returns nil, io.EOF.
 func (dec *Decoder) Token() (json.Token, error) {
-	return dec.dec.Token()
+	ret, err := dec.dec.Token()
+	if err != nil {
+		return nil, err
+	}
+	if dec.useNumber {
+		return ret, nil
+	}
+	if num, ok := ret.(Number); ok {
+		return num.Float64()
+	}
+	return ret, nil
 }
 
 func (dec *Decoder) withErrorContext(err error) error {
